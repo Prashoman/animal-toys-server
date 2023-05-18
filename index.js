b642b3eb769f330ed2f5d147ffe744b99c42ffd6
Badhon <prashomanchakrabarti1@gmail.com>
2023-05-18 10:48:12 +0600

    insert the post toyInfo in db

--- index.js
+++ index.js
@@ -22,6 +22,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db("animalsToysDB").collection("toys");
+
+    app.post("/insert/toys", async (req, res) => {
+      const toys = req.body;
+      console.log(toys);
+      const result = await toyCollection.insertOne(toys);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -29,7 +39,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
